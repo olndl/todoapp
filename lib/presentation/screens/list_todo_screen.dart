b862076130b05828863c653bb7dd041ce2c44f72
Olga Shiryavceva <o.shiryavtseva@yandex.ru>
2022-08-04 02:12:15 +0300

    fixes with design

--- lib/presentation/screens/list_todo_screen.dart
+++ lib/presentation/screens/list_todo_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:intl/intl.dart';
+import '../../core/constants/dimension.dart';
+import '../../core/localization/l10n/all_locales.dart';
 import '../../core/navigation/routes.dart';
 import '../../data/models/todo/todo.dart';
 import '../bloc/list_todo/todos_cubit.dart';
+import '../widgets/app_header.dart';
 
 class TodosScreen extends StatelessWidget {
   @override
@@ -11,15 +14,13 @@ class TodosScreen extends StatelessWidget {
     BlocProvider.of<TodosCubit>(context).fetchTodos();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todos"),
-      ),
       body: BlocBuilder<TodosCubit, TodosState>(
         builder: (context, state) {
           if (!(state is TodosLoaded))
             return Center(child: CircularProgressIndicator());
           final todos = state.todos;
           final revision = state.revision;
+          final scrollController = ScrollController();
           return SingleChildScrollView(
             child: Column(
               children: [
@@ -38,9 +39,245 @@ class TodosScreen extends StatelessWidget {
               ],
             ),
           );
+
+          //   CustomScrollView(
+          //   slivers: [
+          //     _head(),
+          //     _subhead(context),
+          //     _body(scrollController, context, todos, revision),
+          //   ],
+          // );
         },
-      ),);
+      ),
+    );
   }
+
+  Widget _head() {
+    return const SliverPersistentHeader(
+      pinned: true,
+      delegate: AppHeader(),
+    );
+  }
+
+  Widget _subhead(context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: EdgeInsets.only(left: Dim.width(context) / 4.95),
+        child: Text(
+          AllLocale
+              .of(context)
+              .subtitle,
+        ),
+      ),
+    );
+  }
+
+  Widget _body(ScrollController scrollController, context, List<Todo> todos,
+      int revision) {
+    return SliverToBoxAdapter(
+        child: _card(scrollController, context, todos, revision)
+    );
+  }
+
+  Widget _card(ScrollController scrollController, context, List<Todo> todos,
+      int revision) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.only(
+        top: Dim.height(context) / 40,
+        left: Dim.width(context) / 56,
+        right: Dim.width(context) / 56,
+        bottom: Dim.height(context) / 40,
+      ),
+      child:
+      _bodyCard(scrollController, context, todos, revision),);
+  }
+
+
+  Widget _bodyCard(controller, context, List<Todo> todos, int revision) {
+    return ListView.builder(
+      reverse: true,
+      padding: EdgeInsets.only(
+          top: Dim.height(context) / 100, bottom: Dim.height(context) / 100),
+      controller: controller,
+      shrinkWrap: true,
+      itemCount: todos.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = todos[index];
+        return ClipRRect(
+            clipBehavior: Clip.hardEdge,
+            child: _todoBody(index, item, context, revision));
+      },
+    );
+  }
+
+  Widget _todoBody(int index, Todo item, context, int revision) {
+    return Dismissible(
+      key: Key(item.id),
+      background: _fromLeftToRight(),
+      secondaryBackground: _fromRightToLeft(),
+      confirmDismiss: (_) async {
+        BlocProvider.of<TodosCubit>(context).changeCompletion(item, revision);
+        return false;
+      },
+      onDismissed: (direction) =>
+          _toDismissed(direction, context, item, revision),
+      child: _todoTile(item, context, revision),
+    );
+  }
+
+  Widget _fromLeftToRight() {
+    return Container(
+      color: Colors.green,
+      alignment: Alignment.centerLeft,
+      child: const Padding(
+        padding: EdgeInsets.all(15),
+        child: Icon(
+          Icons.check,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+
+  Widget _fromRightToLeft() {
+    return Container(
+      color: Colors.green,
+      alignment: Alignment.centerLeft,
+      child: const Padding(
+        padding: EdgeInsets.all(15),
+        child: Icon(
+          Icons.check,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _confirmDismissal(DismissDirection direction, context,
+      Todo item) async {
+    if (direction == DismissDirection.startToEnd) {
+      return false;
+    } else {
+      bool delete = true;
+      final snackbarController = ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+          Text('${AllLocale
+              .of(context)
+              .deletedTask} ${item.text}'),
+          action: SnackBarAction(
+              label: AllLocale
+                  .of(context)
+                  .undo,
+              onPressed: () => delete = false),
+        ),
+      );
+      await snackbarController.closed;
+      return delete;
+    }
+  }
+
+  Future<bool?> _toDismissed(DismissDirection direction, context, Todo item,
+      int revision) async
+  {
+    if (direction == DismissDirection.startToEnd) {
+      BlocProvider.of<TodosCubit>(context).changeCompletion(item, revision);
+      return false;
+    }
+    if (direction == DismissDirection.endToStart) {
+      BlocProvider.of<TodosCubit>(context).deleteTodo(item, revision);
+    }
+  }
+
+
+  Widget _todoTile(Todo todo, context, int revision) {
+    bool isChanged = todo.done;
+    return ListTile(
+        onTap: () {
+          // context
+          //     .read<NavigationController>()
+          //     .navigateTo(Routes.view, arguments: todo);
+        },
+        leading: Theme(
+          data: Theme.of(context).copyWith(
+            unselectedWidgetColor:
+            todo.importance == "important" ? Colors.red : Colors.grey,
+          ),
+          child: Checkbox(
+            checkColor: Colors.white,
+            activeColor: Colors.green,
+            value: isChanged,
+            onChanged: (_) async {
+              BlocProvider.of<TodosCubit>(context).changeCompletion(
+                  todo, revision);
+              //return false;
+            },
+          ),
+        ),
+        horizontalTitleGap: 3,
+        trailing: IconButton(
+          onPressed: () =>
+              Navigator.pushNamed(context, Routes.EDIT_TODO_ROUTE,
+                  arguments: {"todo": todo, "revision": revision}),
+          icon: const Icon(Icons.info_outline),
+        ),
+        title: todo.done == true
+            ? Text(
+          todo.text,
+          style: const TextStyle(
+              decoration: TextDecoration.lineThrough, color: Colors.grey),
+        )
+            : Row(
+          children: [
+            _priority(todo.importance),
+            Text(
+              todo.text,
+            ),
+          ],
+        ),
+        subtitle: todo.deadline != null
+            ? Text(
+            DateFormat('dd MMMM yyy', 'ru').format(
+                DateTime.fromMillisecondsSinceEpoch(todo.deadline! * 1000)),
+            style: const TextStyle(color: Colors.grey))
+            : null);
+  }
+
+
+  Widget _priority(String? importance) {
+    return importance == "basic"
+        ? const Icon(Icons.arrow_downward)
+        : (importance == "important"
+        ? const Icon(
+      Icons.priority_high_sharp,
+      color: Colors.red,
+    )
+        : const Text(""));
+  }
+
+
+  Widget _btnAdd(int revision, context) {
+    return FloatingActionButton(
+      onPressed: () =>
+          Navigator.pushNamed(context, Routes.ADD_TODO_ROUTE,
+              arguments: revision),
+
+      //     () {
+      //   context
+      //       .read<NavigationController>()
+      //       .navigateTo(Routes.ADD_TODO_ROUTE, arguments: revision);
+      // },
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+
 
   Widget _todo(Todo todo, int revision, context) {
     return InkWell(
@@ -95,7 +332,6 @@ class TodosScreen extends StatelessWidget {
       ),
     );
   }
-}
 
 
 
@@ -357,3 +593,4 @@ class TodosScreen extends StatelessWidget {
 //     );
 //   }
 // }
+
