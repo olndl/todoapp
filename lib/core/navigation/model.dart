9f226afc15fe70594821395c84acf861d02dd7fd
Olga Shiryavceva <o.shiryavtseva@yandex.ru>
2022-08-29 14:40:48 +0300

    delete ptint()

--- lib/core/navigation/model.dart
+++ lib/core/navigation/model.dart
@@ -1,31 +1,23 @@
 import 'dart:convert';
 
-import 'package:todoapp/core/errors/logger.dart';
-
 import '../../domain/model/todo.dart';
 
 typedef JsonMap = Map<String, dynamic>;
 
 abstract class TypedSegment {
   factory TypedSegment.fromJson(JsonMap json) {
-    logger.info('JSON: $json');
     if (json['path'] == 'EditTodoSegment') {
-      logger.info('Зашли в EDit: ${EditTodoSegment(todo: json['todo'])}');
       return EditTodoSegment(todo: json['todo']);
     }
     if (json['path'] == 'ViewTodoSegment') {
-      logger.info('Зашли в View: ${ViewTodoSegment(todo: json['todo'])}');
       return ViewTodoSegment(todo: json['todo']);
     }
     if (json['path'] == 'CreateTodoSegment') {
-      logger.info('Зашли в Create: ${CreateTodoSegment(todo: null)}');
       return CreateTodoSegment(todo: null);
     }
     if (json['path'] == 'ListTodoSegment') {
-      logger.info('Зашли в List: ${ListTodoSegment()}');
       return ListTodoSegment();
     }
-    logger.info('Зашли в List по умолчанию: ${ListTodoSegment()}');
     return ListTodoSegment();
   }
 
