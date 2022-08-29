ee2694b126f661a555246f7e2ed74200c7b7a316
Olga Shiryavceva <o.shiryavtseva@yandex.ru>
2022-08-29 03:41:01 +0300

    update smth

--- lib/presentation/view/components/todo_list_page/todo_list_widget.dart
+++ lib/presentation/view/components/todo_list_page/todo_list_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:todoapp/presentation/view/components/todo_list_page/short_todo_widget.dart';
 import 'package:todoapp/presentation/view/components/todo_list_page/todo_item_card_widget.dart';
 
@@ -18,17 +19,28 @@ class TodoListWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
-        ListView.builder(
-          padding: EdgeInsets.only(
-            top: Dim.height(context) / 100,
-            bottom: Dim.height(context) / 100,
+        AnimationLimiter(
+          child: ListView.builder(
+            padding: EdgeInsets.only(
+              top: Dim.height(context) / 100,
+              bottom: Dim.height(context) / 100,
+            ),
+            controller: ScrollController(),
+            shrinkWrap: true,
+            itemCount: todoList.length,
+            itemBuilder: (_, final int index) {
+              return AnimationConfiguration.staggeredList(
+                position: index,
+                duration: const Duration(milliseconds: 1000),
+                child: SlideAnimation(
+                  verticalOffset: 50.0,
+                  child: FadeInAnimation(
+                    child: TodoItemCardWidget(todo: todoList[index]),
+                  ),
+                ),
+              );
+            },
           ),
-          controller: ScrollController(),
-          shrinkWrap: true,
-          itemCount: todoList.length,
-          itemBuilder: (_, final int index) {
-            return TodoItemCardWidget(todo: todoList[index]);
-          },
         ),
         ShortTodoWidget()
       ],
